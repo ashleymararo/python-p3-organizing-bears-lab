cdd810895926a3088a564f56663534b2443f000c
ashleymararo <ashleymararo222@gmail.com>
2025-11-30 21:31:03 +0300

    finished lab

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 20, 'M', 'dark brown', 'calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 15, 'M', 'black', 'aggressive', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 8, 'F', 'dark brown', 'friendly', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 30, 'M', 'brown', 'strict', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 10, 'F', 'white', 'happy', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 3, 'M', 'green', 'grumpy', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 6, 'F', 'grey', 'gentle', 1);
+
+-- unnamed bear (name is NULL)
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 4, 'M', 'brown', 'neutral', 0);
